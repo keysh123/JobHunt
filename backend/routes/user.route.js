e80af07a197e2baccea963c82d789378b95dfdb2
keysh123 <keyashah126@gmail.com>
2026-02-05 21:57:15 +0100

    adding multer middleware , login , signup connected to backend

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { login, logout, register, updateProfile } from '../controllers/user.controller.js'
 import autheticateToken from "../middleware/isAutheticated.js";
+import { singleUpload } from '../middleware/multer.js';
 const router = express.Router()
-router.post('/register',register)
+router.post('/register',singleUpload, register)
 router.post('/login',login)
-router.post('/profile/update',autheticateToken,updateProfile)
+router.post('/profile/update',autheticateToken,singleUpload,updateProfile)
 router.get('/logout',logout)
 
 export default router;
