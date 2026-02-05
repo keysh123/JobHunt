1378e71f69d0b642ea872ea79f4d7cb9c29d4334
keysh123 <keyashah126@gmail.com>
2026-02-05 21:56:48 +0100

    adding multer middleware , login , signup connected to backend

--- frontend/src/components/authetication/Login.jsx
+++ frontend/src/components/authetication/Login.jsx
@@ -1,55 +1,112 @@
-import React from 'react'
+import React, { useState } from "react";
 import Navbar from "../components_lite/Navbar";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "sonner";
+import { USER_API_ENDPOINT } from "@/utils/data";
 const Login = () => {
+    const navigate= useNavigate()
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
+  const handleChange = async (e) => {
+    setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+   const handleSubmit = async (e) => {
+    e.preventDefault();
+ 
+  
+    try {
+      const res = await axios.post(`${USER_API_ENDPOINT}/login`, data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+      console.log(res);
+      if (res.data.success) {
+        navigate("/");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+        console.log(error.response?.data || error);
+        toast.error(error.response.data?.message)
+    }
+  };
   return (
     <>
-    <Navbar/>
-    <div className="flex items-center justify-center max-w-4xl mx-auto">
-            <form
-              action=""
-              className="w-1/2 border border-gray-300 rounded-md p-4 my-10 "
-            >
-              <h1 className="font-bold text-xl mb-5 text-center">Login</h1>
-             
-              <div className="my-2">
-                <Label>Email</Label>
-                <Input className="mt-2" type="email" placeholder="keyashah@gmail.com"></Input>
-              </div>
-              <div className="my-2">
-                <Label>Password</Label>
-                <Input className="mt-2" type="password" placeholder="********"></Input>
-              </div>
-              
-              <div className="flex items-center justify-start gap-5">
-                <Label>Role</Label>
-                <RadioGroup  className="w-fit flex gap-4 items-center my-5">
-                  <div className="flex items-center gap-3">
-                    <RadioGroupItem value="student" id="r1" />
-                    <Label htmlFor="r1">Student</Label>
-                  </div>
-                  <div className="flex items-center gap-3">
-                    <RadioGroupItem value="recruiter" id="r2" />
-                    <Label htmlFor="r2">Recruiter</Label>
-                  </div>
-                 
-                </RadioGroup>
-              </div>
-            
-              
-              <button className=" my-3 w-full bg-[#022bf8] hover:bg-blue-500 text-white font-medium py-2 px-4 rounded-md mt-5">
-                Login
-              </button>
-              <p className="text-gray-500 text-md my-2">
-               Don't have an account? <Link to="/register" className="text-blue-700">Register</Link>
-              </p>
-            </form>
-          </div>
-    </>
-  )
-}
+      <Navbar />
+      <div className="flex items-center justify-center max-w-4xl mx-auto">
+        <form
+          onSubmit={handleSubmit}
+          className="w-1/2 border border-gray-300 rounded-md p-4 my-10 "
+        >
+          <h1 className="font-bold text-xl mb-5 text-center">Login</h1>
 
-export default Login
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input
+              value={data.email}
+              name="email"
+              onChange={handleChange}
+              className="mt-2"
+              type="email"
+              placeholder="keyashah@gmail.com"
+            ></Input>
+          </div>
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input
+              value={data.password}
+              name="password"
+              onChange={handleChange}
+              className="mt-2"
+              type="password"
+              placeholder="********"
+            ></Input>
+          </div>
+
+          <div className="flex items-center gap-5">
+            <Label className="whitespace-nowrap">Role</Label>
+
+            <RadioGroup
+              value={data.role}
+              onValueChange={(value) =>
+                setData((prev) => ({ ...prev, role: value }))
+              }
+              className="flex gap-4 items-center my-5"
+            >
+              <div className="flex items-center gap-3">
+                <RadioGroupItem value="student" id="r1" />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+
+              <div className="flex items-center gap-3">
+                <RadioGroupItem value="recruiter" id="r2" />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+
+          <button type="submit" className=" my-3 w-full bg-[#022bf8] hover:bg-blue-500 text-white font-medium py-2 px-4 rounded-md mt-5">
+            Login
+          </button>
+          <p className="text-gray-500 text-md my-2">
+            Don't have an account?{" "}
+            <Link to="/register" className="text-blue-700">
+              Register
+            </Link>
+          </p>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- frontend/src/components/authetication/Register.jsx
+++ frontend/src/components/authetication/Register.jsx
@@ -1,60 +1,156 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components_lite/Navbar";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { USER_API_ENDPOINT } from "@/utils/data";
+import { toast } from "sonner";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    role: "",
+    phoneNo: "",
+    file: "",
+  });
+  const handleChange = async (e) => {
+    setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const handleFileChange = async (e) => {
+    setData((prev) => ({ ...prev, file: e.target.files?.[0] }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("fullName", data.fullName);
+    formData.append("email", data.email);
+    formData.append("password", data.password);
+    formData.append("role", data.role);
+    formData.append("phoneNo", data.phoneNo);
+    if (data.file) {
+      formData.append("file", data.file);
+    }
+
+    // console.log(data);
+    try {
+      const res = await axios.post(`${USER_API_ENDPOINT}/register`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        withCredentials: true,
+      });
+      console.log(res);
+      if (res.data.success) {
+        navigate("/login");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+        console.log(error.response.data);
+        toast.error(error.response.data?.message)
+     
+    }
+  };
   return (
     <>
       <Navbar />
       <div className="flex items-center justify-center max-w-4xl mx-auto">
         <form
-          action=""
+          onSubmit={handleSubmit}
           className="w-1/2 border border-gray-300 rounded-md p-4 my-10 "
         >
           <h1 className="font-bold text-xl mb-5 text-center">Register</h1>
           <div className="my-2">
             <Label>Name</Label>
-            <Input className="mt-2" type="text" placeholder="Keya Shah"></Input>
+            <Input
+              value={data.fullName}
+              onChange={handleChange}
+              name="fullName"
+              className="mt-2"
+              type="text"
+              placeholder="Keya Shah"
+            ></Input>
           </div>
           <div className="my-2">
             <Label>Email</Label>
-            <Input className="mt-2" type="email" placeholder="keyashah@gmail.com"></Input>
+            <Input
+              value={data.email}
+              onChange={handleChange}
+              name="email"
+              className="mt-2"
+              type="email"
+              placeholder="keyashah@gmail.com"
+            ></Input>
           </div>
           <div className="my-2">
             <Label>Password</Label>
-            <Input className="mt-2" type="password" placeholder="********"></Input>
+            <Input
+              value={data.password}
+              onChange={handleChange}
+              name="password"
+              className="mt-2"
+              type="password"
+              placeholder="********"
+            ></Input>
           </div>
           <div className="my-2">
             <Label>Phone No</Label>
-            <Input className="mt-2" type="number" placeholder="1234567809"></Input>
+            <Input
+              value={data.phoneNo}
+              onChange={handleChange}
+              name="phoneNo"
+              className="mt-2"
+              type="number"
+              placeholder="1234567809"
+            ></Input>
           </div>
-          <div className="flex items-center justify-start gap-5">
-            <Label>Role</Label>
-            <RadioGroup  className="w-fit flex gap-4 items-center my-5">
+          <div className="flex items-center gap-5">
+            <Label className="whitespace-nowrap">Role</Label>
+
+            <RadioGroup
+              value={data.role}
+              onValueChange={(value) =>
+                setData((prev) => ({ ...prev, role: value }))
+              }
+              className="flex gap-4 items-center my-5"
+            >
               <div className="flex items-center gap-3">
                 <RadioGroupItem value="student" id="r1" />
                 <Label htmlFor="r1">Student</Label>
               </div>
+
               <div className="flex items-center gap-3">
                 <RadioGroupItem value="recruiter" id="r2" />
                 <Label htmlFor="r2">Recruiter</Label>
               </div>
-             
             </RadioGroup>
           </div>
+
           <div className="flex items-center gap-2">
             <Label className="whitespace-nowrap">Profile Photo</Label>
-            <Input type="file" accept="image/*" className="cursor-pointer"></Input>
+            <Input
+              type="file"
+              accept="image/*"
+              onChange={handleFileChange}
+              className="cursor-pointer"
+            ></Input>
           </div>
-          
-          <button className=" my-3 w-full bg-[#022bf8] hover:bg-blue-500 text-white font-medium py-2 px-4 rounded-md mt-5">
+
+          <button
+            type="submit"
+            className=" my-3 w-full bg-[#022bf8] hover:bg-blue-500 text-white font-medium py-2 px-4 rounded-md mt-5"
+          >
             Register
           </button>
           <p className="text-gray-500 text-md my-2">
-            Already have an account? <Link to="/login" className="text-blue-700">Login</Link>
+            Already have an account?{" "}
+            <Link to="/login" className="text-blue-700">
+              Login
+            </Link>
           </p>
         </form>
       </div>
